9e788e936376ca8e27f217a72ecedd6b1c678a2e
treerink <tjreerink@gmail.com>
2018-02-21 14:12:07 +0100

    Add a tiny script to merge the ece dr2xml patch into the dr2xml repository.

--- diff-patch-repo-with-original-dr2xml-repo.sh
+++ diff-patch-repo-with-original-dr2xml-repo.sh
@@ -9,15 +9,23 @@
 
 if [ "$#" -eq 0 ]; then
  dir_patch_repo=${HOME}/cmorize/dr2xml/ece-dr2xml-patch
- dir_original_dr2xml_repo=${HOME}/cmorize/dr2xml/dr2xml-repository
+ dir_original_dr2xml_repo=${HOME}/cmorize/dr2xml/dr2xml-repository-test
 
- diff ${dir_patch_repo}/Xparse.py                ${dir_original_dr2xml_repo}/Xparse.py
- diff ${dir_patch_repo}/ece_create_ping_files.py ${dir_original_dr2xml_repo}/ece_create_ping_files.py
- diff ${dir_patch_repo}/dr2xml.py                ${dir_original_dr2xml_repo}/dr2xml.py
- diff ${dir_patch_repo}/ece_dr2xml.py            ${dir_original_dr2xml_repo}/ece_dr2xml.py
+ diff ${dir_patch_repo}/dr2xml.py                  ${dir_original_dr2xml_repo}/dr2xml.py
+ diff ${dir_patch_repo}/Xparse.py                  ${dir_original_dr2xml_repo}/Xparse.py
+ diff ${dir_patch_repo}/ece_create_ping_files.py   ${dir_original_dr2xml_repo}/ece_create_ping_files.py
+ diff ${dir_patch_repo}/ece_dr2xml.py              ${dir_original_dr2xml_repo}/ece_dr2xml.py
  
  cd ${dir_original_dr2xml_repo}; git diff > change-log-of-hacks.txt; cd ${dir_patch_repo};
- diff ${dir_patch_repo}/change-log-of-hacks.txt  ${dir_original_dr2xml_repo}/change-log-of-hacks.txt 
+ diff ${dir_patch_repo}/change-log-of-hacks.txt    ${dir_original_dr2xml_repo}/change-log-of-hacks.txt 
+
+ if [ -e ${dir_original_dr2xml_repo}/ping_nemo.xml ]; then
+  diff ${dir_patch_repo}/benchmark/ping_nemo.xml   ${dir_original_dr2xml_repo}/ping_nemo.xml
+ fi
+ if [ -e ${dir_original_dr2xml_repo}/dr2xml_nemo.xml ]; then
+  diff ${dir_patch_repo}/benchmark/dr2xml_nemo.xml ${dir_original_dr2xml_repo}/dr2xml_nemo.xml
+ fi
+
 
 else
     echo '  '
---
+++ merge-patch-repo-with-original-dr2xml-repo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Thomas Reerink
+#
+# This scripts needs no arguments
+#
+# Run example:
+#  ./merge-patch-repo-with-original-dr2xml-repo.sh
+#
+
+if [ "$#" -eq 0 ]; then
+ dir_patch_repo=${HOME}/cmorize/dr2xml/ece-dr2xml-patch
+ dir_original_dr2xml_repo=${HOME}/cmorize/dr2xml/dr2xml-repository-test
+
+ cp ${dir_patch_repo}/dr2xml.py                    ${dir_original_dr2xml_repo}/dr2xml.py
+ cp ${dir_patch_repo}/Xparse.py                    ${dir_original_dr2xml_repo}/Xparse.py
+ cp ${dir_patch_repo}/ece_create_ping_files.py     ${dir_original_dr2xml_repo}/ece_create_ping_files.py
+ cp ${dir_patch_repo}/ece_dr2xml.py                ${dir_original_dr2xml_repo}/ece_dr2xml.py
+ 
+ ./diff-patch-repo-with-original-dr2xml-repo.sh
+ 
+ echo '  '
+ echo ' If the Shaconemo repository is in place, you should be able to run ece_create_ping_files.py by:'
+ echo '  cd' ${dir_original_dr2xml_repo}
+ echo '  python ece_create_ping_files.py'
+ echo '  '
+ echo ' And ece_dr2xml.py should be able to run by:'
+ echo '  python ece_dr2xml.py'
+ echo '  '
+
+else
+    echo '  '
+    echo '  Illegal number of arguments, e.g.:'
+    echo '  ' $0
+    echo '  '
+fi
